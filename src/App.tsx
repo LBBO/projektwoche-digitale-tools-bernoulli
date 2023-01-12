bab064dc9d0b3015db9922184673c6df8c9478c1
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-01-12 13:52:38 +0100

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [probability, setProbability] = useState(0.5)
+  const [forceFair, setForceFair] = useState(true)
+  const finalProbability = forceFair ? 0.5 : probability
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'card'}>
+      test
+      <label>
+        <input
+          type="checkbox"
+          checked={forceFair}
+          onChange={() => setForceFair(!forceFair)}
+        />
+        Fair
+      </label>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
