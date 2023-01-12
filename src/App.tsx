27b3ac5e5bbf8f3678946afe29a2dc1128482ede
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-01-12 13:52:38 +0100

    Create form for probabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import React, { useState } from 'react'
+import React, { HTMLProps, useState } from 'react'
 import './App.css'
 
+const useNumberInput = (
+  defaultValue: number,
+  inputProps: HTMLProps<HTMLInputElement> = {},
+) => {
+  const [numberValue, setNumberValue] = useState(defaultValue)
+  const [value, setValue] = useState(numberValue.toString())
+  const component = (
+    <input
+      {...inputProps}
+      type={'number'}
+      value={value}
+      onChange={(e) => {
+        setValue(e.target.value)
+        const parsed = parseFloat(e.target.value)
+        console.log(e.target.value, parsed)
+        if (!Number.isNaN(parsed)) {
+          setNumberValue(parsed)
+        }
+      }}
+    />
+  )
+  return [numberValue, component]
+}
+
 function App() {
-  const [probability, setProbability] = useState(0.5)
   const [forceFair, setForceFair] = useState(true)
+  const [probability, probabilityInput] = useNumberInput(0.5, {
+    disabled: forceFair,
+  })
   const finalProbability = forceFair ? 0.5 : probability
 
   return (
     <div className={'card'}>
-      test
-      <label>
-        <input
-          type="checkbox"
-          checked={forceFair}
-          onChange={() => setForceFair(!forceFair)}
-        />
-        Fair
-      </label>
+      <div className="coin"></div>
+      <div className="options">
+        <label>
+          {probabilityInput}
+          Wahrscheinlichkeit
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={forceFair}
+            onChange={() => setForceFair(!forceFair)}
+          />
+          Fair
+        </label>
+      </div>
     </div>
   )
 }
